98741b0ed02b50913fd73dc65b628ab5ca7376bb
tomirausch <trausch@frsf.utn.edu.ar>
2026-06-02 12:12:57 -0300

    Inicio de proyecto

---
+++ src/main/java/tp/metodosAgiles/gestionLicencias/GestionLicenciasApplication.java
@@ -0,0 +1,13 @@
+package tp.metodosAgiles.gestionLicencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionLicenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionLicenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tp/metodosAgiles/gestionLicencias/GestionLicenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package tp.metodosAgiles.gestionLicencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionLicenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
